12198413907106acb979b7f9954504cfd5bac6a5
Aleksandr Senchenko <awb.senchenko@gmail.com>
2020-08-29 21:26:22 +0300

    init and description

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,27 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+const {ipcRenderer} = require('electron')
+
+const selectDirBtn = document.getElementById("select-asterios-path")
+
+selectDirBtn.addEventListener('click', (_) => {
+    ipcRenderer.send('open-file-dialog')
+})
+
+ipcRenderer.on('selected-directory', (event, path) => {
+    document.getElementById("asterios-path").innerHTML = path.toString();
+    selectDirBtn.remove();
+})
+
+const askOnLoad = ipcRenderer.sendSync('synchronous-message', ["asterios-path-specified"])
+let command = askOnLoad.toString();
+if (askOnLoad instanceof Array) command = askOnLoad[0];
+switch (command) {
+    case "asterios-path-specified":
+        document.getElementById("asterios-path").innerHTML = askOnLoad[1];
+        selectDirBtn.remove();
+        break;
+    case "asterios-path-not-specified":
+        break;
+}
